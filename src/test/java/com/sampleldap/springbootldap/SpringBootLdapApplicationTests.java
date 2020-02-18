5948ea9f38120c42718f78e011836d379551d845
Milan Desai <milan.desai@smartstream-stp.com>
2020-02-18 14:08:31 +0000

    Test ldap server

---
+++ src/test/java/com/sampleldap/springbootldap/SpringBootLdapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sampleldap.springbootldap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLdapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
